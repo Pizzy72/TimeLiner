ca6d2af81cefa1a7e751b584dbedd37fa5ffce62
Christian Pistor <cpist@mailbox.org>
2026-08-30 20:35:13 +0200

    Update date

--- Source/TimeLiner/App.xaml.cs
+++ Source/TimeLiner/App.xaml.cs
@@ -1,5 +1,5 @@
-﻿// SPDX-License-Identifier: MIT
-// Copyright (c) 2021–2025 Christian Pistor
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2021–2026 Christian Pistor
 
 using System.Windows;
 using System.Windows.Controls;
